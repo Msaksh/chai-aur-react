271736483ed37ad2cb242d1af9618d96d0b25a45
Msaksh <sakshamm@beelabs.technology>
2025-08-04 18:26:52 +0530

    hashrouter used instead of browser router

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 
 import './App.css'
-import { Routes, Route, Link } from 'react-router-dom';
+import { Routes, Route, Link, BrowserRouter, HashRouter } from 'react-router-dom';
 import BgChanger from './pages/bgChanger/BgChanger';
 import Home from './pages/Home/Home';
 
@@ -9,12 +9,13 @@ function App() {
 
   return (
     <>
-
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/bgchanger" element={<BgChanger />} />
-        {/* Add more routes as needed */}
-      </Routes>
+      <HashRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/bgchanger" element={<BgChanger />} />
+          {/* Add more routes as needed */}
+        </Routes>
+      </HashRouter>
 
     </>
   )
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -16,16 +16,23 @@ function Home() {
 
 
   return (
-    <div>
+    <div className='flex flex-col items-center justify-center h-screen w-full bg-gray-400'>
       <h1>Count is : {count}</h1>
       <div className="card">
-        <button onClick={addValues}>
+        <button onClick={addValues}
+          className='rounded bg-green-400 text-white p-2 hover:bg-red-500 transition-all duration-300'>
           add count
         </button>
         <br />
-        <button onClick={removeValues}>
+        <button className='rounded-full bg-green-500 text-white p-2 hover:bg-red-500 transition-all duration-300' onClick={removeValues}>
           remove count
         </button>
+        <a href="/chai-aur-react/#/bgchanger" className='mt-4'>
+
+          <button className='cursor-pointer rounded bg-blue-500 text-white p-2 hover:bg-blue-600 transition-all duration-300'>
+            BgChanger
+          </button>
+        </a>
 
       </div>
     </div>
